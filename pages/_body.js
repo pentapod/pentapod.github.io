27531f2094eda88a9be8e5309a3ee194833a0cff
spring-raining <fdonthavei@gmail.com>
2017-01-12 05:53:06 +0900

    Initial commit

---
+++ pages/_body.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Head from 'next/head';
+
+class Body extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  static defaultProps = {
+    siteName: 'pentapod',
+    title: 'pentapod',
+    description: 'かゆいところに手が届くソフトウェア技術系同人サークル「pentapod」のHPです。',
+    url: 'https://pentapod.github.io',
+    image: 'https://pentapod.github.io/static/og.png',
+    favicon: '/static/favicon.ico',
+  };
+
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>{this.props.title}</title>
+
+          <meta property="og:site_name" content={this.props.siteName} />
+          <meta property="og:title" content={this.props.title} />
+          <meta property="og:url" content={this.props.url} />
+          <meta property="og:image" content={this.props.image} />
+          <meta property="og:description" content={this.props.description} />
+          <meta property="og:type" content="website" />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:creator" content="@spring_raining" />
+          <meta name="google-site-verification" content="uf35tScRk5eGKgvnTasid2iYy3Z3TdPrqFC7rgLsfjs" />
+
+          <link rel="shortcut icon" href={this.props.favicon} />
+          <link rel="canonical" href={this.props.url} />
+
+          <script dangerouslySetInnerHTML={{__html:
+            `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-46915388-6', 'auto');
+            ga('send', 'pageview');
+            `}}
+          />
+        </Head>
+
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Body;
---
+++ pages/index.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import Isvg from 'react-inlinesvg';
+
+import Body from './_body';
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textColor: `hsl(${Math.random() * 360}, 27%, 34%)`,
+    };
+  }
+
+  render() {
+    return (
+      <Body>
+        <div className="container" style={{ color: this.state.textColor }}>
+          <div className="container-left">
+            <div className="container-brand">
+              <div className="logo"
+                   style={{ width: 50, fill: this.state.textColor }}
+                   >
+                <Isvg src='/static/logo.svg'></Isvg>
+              </div>
+              <div className="brand"
+                   style={{ width: 200, fill: this.state.textColor }}
+                   >
+                <Isvg src='/static/brand.svg'>pentapod</Isvg>
+              </div>
+            </div>
+          </div>
+
+          <div className="container-right">
+            <section>
+              <header><h2>About pentapod</h2></header>
+              <p>
+                pentapodは、ソフトウェア技術についての同人誌を制作する同人サークルです。
+              </p>
+            </section>
+            <section>
+              <header><h2>Member</h2></header>
+              <ul>
+                <li>緑豆はるさめ(<a href="https://twitter.com/spring_raining" target="_blank">@spring_raining</a>)</li>
+              </ul>
+            </section>
+            <section>
+              <p>
+                ↓ご連絡はこちらまで↓<br />
+                <a href="mailto:harusamex.com@gmail.com">harusamex.com@gmail.com</a>
+              </p>
+            </section>
+          </div>
+        </div>
+
+        <style jsx global>{`
+          body {
+            background-color: #fff;
+            font-size: 16px;
+            font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+            font-weight: 200;
+          }
+
+          .isvg {
+            transition: opacity 1s;
+            opacity: 0;
+          }
+          .isvg.loaded {
+            opacity: 1;
+          }
+        `}</style>
+
+        <style jsx>{`
+          .container {
+            display: flex;
+            max-width: 1080px;
+            margin: 0 auto;
+            padding-top: 10em;
+          }
+          .container-left {
+            flex-basis: 300px;
+            padding: 0 1em;
+          }
+          .container-right {
+            flex-grow: 1;
+            padding: 0 1em;
+          }
+          .container-brand {
+            text-align: right;
+          }
+          .logo, .brand {
+            display: block;
+            margin-left: auto;
+            padding-top: 1em;
+          }
+
+          @media (max-width: 640px) {
+            .container {
+              flex-direction: column;
+              padding: 0 .5em;
+            }
+            .container-left {
+              flex-basis: 150px;
+              padding: 0;
+            }
+            .container-right {
+              padding: 0;
+            }
+            .container-brand {
+              padding-right: 3em;
+            }
+          }
+        `}</style>
+      </Body>
+    );
+  }
+}
